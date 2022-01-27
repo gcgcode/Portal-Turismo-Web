d99db247ea21e5e9f50e0c294ec0c0c588ed8441
Borja <borjagonzalezhidalgo@gmail.com>
2022-01-27 19:01:08 +0100

    Solucion bug generateRandomString() #En ocasiones arrojaba un punto "."

--- js.js
+++ js.js
@@ -18,16 +18,21 @@ $(function() {
 });
 
 const  generateRandomString = (num) => {
-	const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	let result1= (Math.random()*100000000000000000).toString(36).substring(0,num);    
-	return result1;
+	var resultado="";
+	 let result1= (Math.random()*1000000000000000000).toString(36).substring(0,num);
+	 for (let index = 0; index < result1.length; index++) {
+		 if(result1.charAt(index)!='.'){
+		 resultado=resultado+result1.charAt(index);
+		 }
+	 }
+	 return resultado;
 }
 
 function sendEmail(e){
 	Email.send({
 		Host:"smtp.gmail.com",
 		Username:"bgh0001@alu.medac.es",
-		Password:"***********",
+		Password:"Amxmodmenu1234",
 		To: e,
 		From: "bgh0001@alu.medac.es",
 		Subject:"Sending Email Using JS",
