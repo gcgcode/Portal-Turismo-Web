486dff74ec1e1a6f6cf5c6e9c35621e21c8bec3b
Borja <borjagonzalezhidalgo@gmail.com>
2022-01-27 18:34:49 +0100

    GenerateRandomString() + SMTP

--- js.js
+++ js.js
@@ -16,3 +16,25 @@ $(function() {
 	});
 
 });
+
+const  generateRandomString = (num) => {
+	const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let result1= (Math.random()*100000000000000000).toString(36).substring(0,num);    
+	return result1;
+}
+
+function sendEmail(e){
+	Email.send({
+		Host:"smtp.gmail.com",
+		Username:"bgh0001@alu.medac.es",
+		Password:"***********",
+		To: e,
+		From: "bgh0001@alu.medac.es",
+		Subject:"Sending Email Using JS",
+		Body: `Tu nueva contraseña es: ${generateRandomString(30)}`,
+	})
+	.then(function(message){
+		alert("mail sent successfully");
+		
+	});
+}
