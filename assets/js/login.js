f27d14d5f95fdbc5ac8f252b9ffdf6784b02483b
Jesus <jejisou02@gmail.com>
2022-02-09 11:57:18 +0100

    JQuery

--- assets/js/login.js
+++ assets/js/login.js
@@ -19,8 +19,72 @@ $(function() {
 $(document).ready(function() {
   $("#login-form").validate({
     rules: {
-      "usernameLogin" : {required:true, minlength:3, maxlength:15, nowhitespace, alphanumeric, pattern: /^[A-Za-z0-9]{5,20}/},
-      "passwordLogin" : {required:true, minlength:6, maxlength:15, nowhitespace, pattern: /^(?=.\d)(?=.[a-z])(?=.[A-Z]).{8,}/}
-    }
+      username : {
+        required:true, 
+        minlength:3, 
+        maxlength:15,
+        nowhitespace: true,
+        alphanumeric: true,
+      },
+      password : {
+        required:true,
+         minlength:6, 
+         maxlength:15,
+         nowhitespace: true,
+      }
+    },
+    messages: {
+      username: {
+        required:"Introduce el usuario",
+        minlength:"El usuario debe tener almenos 3 caracteres",
+        maxlength:"El usuario no puede contener más de 15 caracteres",
+        nowhitespace:"No puede contener espacios en blanco",
+        alphanumeric:"El usuario solo puede contener numeros y letras"
+      },
+      password: {
+        required:"Introduce la contraseña",
+        minlength:"La contraseña debe de tener almenos 6 caracteres",
+        maxlength:"La contraseña no puede contener más de 15 caracteres",
+        nowhitespace:"La contraseña solo puede contener numeros y letras"
+      }
+    } 
+  });
+});
+
+$(document).ready(function() {
+  $("#register-form").validate({
+    rules: {
+      username: {
+        required:true, 
+        minlength:3, 
+        maxlength:15,
+        nowhitespace: true,
+        alphanumeric: true,
+      },
+      email: {
+        email: true
+      },
+      password: {
+        required:"Introduce la contraseña",
+        minlength:"La contraseña debe de tener almenos 6 caracteres",
+        maxlength:"La contraseña no puede contener más de 15 caracteres",
+        nowhitespace:"La contraseña solo puede contener numeros y letras"
+      }
+    },
+    messages: {
+      username: {
+        required:"Introduce el usuario",
+        minlength:"El usuario debe tener almenos 3 caracteres",
+        maxlength:"El usuario no puede contener más de 15 caracteres",
+        nowhitespace:"No puede contener espacios en blanco",
+        alphanumeric:"El usuario solo puede contener numeros y letras"
+      },
+      password: {
+        required:"Introduce la contraseña",
+        minlength:"La contraseña debe de tener almenos 6 caracteres",
+        maxlength:"La contraseña no puede contener más de 15 caracteres",
+        nowhitespace:"La contraseña solo puede contener numeros y letras"
+      }
+    } 
   });
 });
