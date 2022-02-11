3114e9e027b17fd6f2ca6d6124cde73debfc6ec8
gcgcode <gnzlcantosies@gmail.com>
2022-02-11 12:55:12 +0100

    AJAX -> insertar coordenadas

---
+++ assets/js/insertCoordenada.js
@@ -0,0 +1,21 @@
+function insert() {
+    var ajax = new XMLHttpRequest();
+      ajax.open("GET", "maps_api.php?id_categoria=monumento", true);
+      ajax.send();
+      let data;
+
+  ajax.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+              data = JSON.parse(this.responseText);
+                console.log(data);
+              var getElement = document.getElementById('fila');
+              for (let i = 0; i < data.length; i++) {
+                var titulo =  data[i].TITULO;
+                var img = data[i].IMG;
+                var div = '<div class="col-lg-4 text-center"><img src="'+img+'" width="300" height="200" alt=""><h3>'+titulo+'</h3></div>';
+                getElement.insertAdjacentHTML( 'beforeend', div );
+             
+              }
+          }
+  }
+}
