22b0017a163717f24e255deb8bc8b92a7789f4ff
gcgcode <gnzlcantosies@gmail.com>
2022-02-11 16:29:25 +0100

    AJAX -> CRUD coordenada

--- assets/js/insertCoordenada.js
+++ assets/js/insertCoordenada.js
@@ -1,21 +1,27 @@
 function insert() {
-    var ajax = new XMLHttpRequest();
-      ajax.open("GET", "maps_api.php?id_categoria=monumento", true);
-      ajax.send();
-      let data;
 
-  ajax.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-              data = JSON.parse(this.responseText);
-                console.log(data);
-              var getElement = document.getElementById('fila');
-              for (let i = 0; i < data.length; i++) {
-                var titulo =  data[i].TITULO;
-                var img = data[i].IMG;
-                var div = '<div class="col-lg-4 text-center"><img src="'+img+'" width="300" height="200" alt=""><h3>'+titulo+'</h3></div>';
-                getElement.insertAdjacentHTML( 'beforeend', div );
-             
-              }
-          }
-  }
+    var categoria = document.getElementById("categoria").value;
+    var latitud = document.getElementById("latitud").value;
+    var longitud = document.getElementById("longitud").value;
+    var direccion = document.getElementById("direccion").value;
+    var titulo = document.getElementById("titulo").value;
+    var descripcion = document.getElementById("descripcion").value;
+    var img = document.getElementById("img").value;
+
+    var ajax = new XMLHttpRequest();
+      ajax.open("POST", "../maps_api.php", true);
+
+      let formData = new FormData();
+
+        formData.append('latitud', latitud);
+        formData.append('longitud', longitud);
+        formData.append('direccion', direccion);
+        formData.append('titulo', titulo);
+        formData.append('descripcion', descripcion);
+        formData.append('telefono', descripcion);
+        formData.append('img', img);
+        formData.append('id_categoria', categoria);
+
+      ajax.send(formData);
+     
 }
