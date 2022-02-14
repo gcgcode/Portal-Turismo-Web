fa0aacda71e465567955709de9872a3b27fd80dc
Borja <borjagonzalezhidalgo@gmail.com>
2022-02-14 13:41:44 +0100

    MANITA_ENRIQUE

--- assets/js/getMonumento.js
+++ assets/js/getMonumento.js
@@ -1,19 +1,22 @@
 window.onload = function () {
-    
+  console.log("Hola");
     var ajax = new XMLHttpRequest();
-      ajax.open("GET", "../../../proyectoFinal/php/api/maps_api.php?id_coordenada=titulo", true);
+      console.log(window.location.search);
+      const urlParams = new URLSearchParams(window.location.search);
+      var urldest="../../../proyectoFinal/php/api/maps_api.php?id_coordenada=" + urlParams.get('id');
+      ajax.open("GET", urldest, true);
       ajax.send();
       let data;
       
   ajax.onreadystatechange = function() {
           if (this.readyState == 4 && this.status == 200) {
               data = JSON.parse(this.responseText);
-               
               var titulo = document.getElementById('tituloMonumento');
               var direccion = document.getElementById('idDireccionMonumento');
               var telefono = document.getElementById('idTelefonoMonumento');
               var horario = document.getElementById('idHorarioMonumento');
               var img = document.getElementById('imgMonumento');
+
               var texto = document.getElementById('textoMonumento');
 
 
@@ -23,18 +26,24 @@ window.onload = function () {
                 var valorDireccion =  data[i].DIRECCION;
                 var valorTelefono =  data[i].TELEFONO;
                 var valorHorario =  data[i].HORARIO;
-                var valorImg = data[i].IMG;
-                var valorTexto =  data[i].TEXTO;
-
+                var valorTexto =  data[i].DESCRIPCION;
+                var valorImagen =  data[i].IMG; 
+                var linea = "<img src='"+valorImagen+"' srcset='' width='600' height='400'>";
+                img.insertAdjacentHTML(
+                    "beforeend",
+                    linea // Backticks para img variable
+                  );
+                
                 titulo.innerHTML=valorTitulo;
-                direccion.innerHTML=valorDireccion;
-                telefono.innerHTML=valorTelefono;
-                horario.innerHTML=valorHorario;
-                img.innerHTML=valorImg;
+                direccion.innerHTML="Dirección: "+valorDireccion;
+                telefono.innerHTML="Teléfono: "+valorTelefono;
+                horario.innerHTML="Horario: "+valorHorario;
+
                 texto.innerHTML=valorTexto;
 
              
               }
+              
           }
         }
     }
--- assets/js/getMonumentos.js
+++ assets/js/getMonumentos.js
@@ -13,7 +13,10 @@ window.onload = function () {
               for (let i = 0; i < data.length; i++) {
                 var titulo =  data[i].TITULO;
                 var img = data[i].IMG;
-                var div = '<div class="col-lg-4 text-center"><img src="'+img+'" width="300" height="200" alt=""><h3>'+titulo+'</h3></div>';
+                var id = data[i].ID_COORDENADA;
+                id = id +"";
+                console.log(id);
+                var div = '<div class="col-lg-4 text-center"><img src="'+img+'" width="300" height="200" alt="" onclick= window.open("./monumento.html?id='+ id+'")><h3>'+titulo+'</h3></div>';
                 getElement.insertAdjacentHTML( 'beforeend', div );
              
               }
